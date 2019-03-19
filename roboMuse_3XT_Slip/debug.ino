e38b33090a132d2192600e7483e500290e8fb38d
Hetarth Chopra <34271010+choprahetarth@users.noreply.github.com>
2019-03-19 18:49:48 +0530

    updated
    no need as of now

--- roboMuse_3XT_Slip/debug.ino
+++ roboMuse_3XT_Slip/debug.ino
@@ -2,8 +2,8 @@ int time=0;
 float filteredTheta,deltaFilteredTheta;
 float lastAngle;
 double lastX , lastY;
-void debugMode(){
 
+void debugMode(){
   while(time<3000 && interApt == 100){
     time = millis();
     safeCheck();        ///// press a to exit the loop
@@ -20,8 +20,7 @@ void debugMode(){
     PID_L.Compute(); PID_R.Compute();
     leftMotorSpeed += outputL;
     rightMotorSpeed -= outputR;
-     Serial.println(filteredTheta);
-    x = x + centreIncremental * cos(filteredTheta + deltaFilteredTheta / 2)*(-1);   ///FILTERED XY
+    x = x + centreIncremental * cos(filteredTheta + deltaFilteredTheta / 2)*(-1);   ///FILTERED XY///
     y = y + centreIncremental * sin(filteredTheta + deltaFilteredTheta / 2);
     saberTooth.motor(1, leftMotorSpeed);
     saberTooth.motor(2, rightMotorSpeed);
@@ -29,22 +28,29 @@ void debugMode(){
     saberTooth.stop();
     delay(1000);
     //Serial.println("I am out");
+    readOnceVariable = 1;
     lastAngle = filteredTheta;
     lastX = x;
     lastY = y;
-   while (abs(filteredTheta) <30){
-    Serial.println("entered this loop");
-        //imuRead();          //// to start reading the IMU 
-        //angleProcessing();
+   while (time < 10000){
+    Serial.println("i am here...");
+        
+        imuRead();          //// to start reading the IMU 
+        angleProcessing();
         odometryCalc();
         slipDetection();
-         x = lastX + x + centreIncremental * cos(lastAngle+ filteredTheta + deltaFilteredTheta / 2)*(-1);   ///FILTERED XY
-         y = lastY + y + centreIncremental * sin(lastAngle+ filteredTheta + deltaFilteredTheta / 2);
+         x =  x + centreIncremental * cos( filteredTheta + deltaFilteredTheta / 2)*(-1);   ///FILTERED XY
+         y =  y + centreIncremental * sin( filteredTheta + deltaFilteredTheta / 2);
         calSpeed(-30, 30, filteredTheta);
         saberTooth.motor(1, -speedTurn-5);
         saberTooth.motor(2, speedTurn+5);
-        Serial.println(filteredTheta);
+        //Serial.println(filteredTheta);
     }
-
+    /*Serial.print(",");
+    Serial.println(lastX + x);
+    Serial.print(",");
+    Serial.print(lastY +y);
+    Serial.print(",");
+    Serial.print(lastAngle+filteredTheta);*/
    
   }
