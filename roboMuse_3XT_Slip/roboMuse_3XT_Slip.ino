4425a11cbf19e70aef355e9d0fbee149ab7b798a
Hetarth Chopra <34271010+choprahetarth@users.noreply.github.com>
2019-03-27 12:51:09 +0530

    UPDATED

--- roboMuse_3XT_Slip/roboMuse_3XT_Slip.ino
+++ roboMuse_3XT_Slip/roboMuse_3XT_Slip.ino
@@ -118,8 +118,8 @@ void velocityApproximation(){
   velocityRightWheel = rightWheelIncrement / dt;
   centreWheelVelocity = (velocityLeftWheel + velocityRightWheel)/2;
   
-  //Serial.print("new velocity:");
-  //Serial.println(centreWheelVelocity);
+    //Serial.print("new velocity:");
+    //Serial.println(centreWheelVelocity);
 
 //  Serial.print("accurate velocity:");
 //  Serial.println(centreWheelVelocity*2);
