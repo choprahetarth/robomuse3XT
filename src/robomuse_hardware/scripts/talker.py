b27f7b3bc856242422c90e34b933c00034f96b9c
choprahetarth <choprahetarth@gmail.com>
2019-04-10 12:54:37 +0530

    "added the mpu6050 and the talker and listener for the fusion of the two angles "

---
+++ src/robomuse_hardware/scripts/talker.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import Float32
+
+def anglePublisher():
+    pub = rospy.Publisher('normalTheta', Float32, queue_size=20)
+    rospy.init_node('angle1', anonymous=True)
+    rate = rospy.Rate(20) # 10hz
+    while not rospy.is_shutdown():
+        angleNormal = 10
+        rospy.loginfo(angleNormal)
+        pub.publish(angleNormal)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        anglePublisher()
+    except rospy.ROSInterruptException:
+        pass
+
