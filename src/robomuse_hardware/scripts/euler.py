62799ba13c038f11757ad01db82d1987194d0b8a
choprahetarth <choprahetarth@gmail.com>
2019-04-11 21:53:16 +0530

    "added the subscriber and publisher of euler angles"

--- src/robomuse_hardware/scripts/euler.py
+++ src/robomuse_hardware/scripts/euler.py
@@ -6,33 +6,50 @@ from sensor_msgs.msg import Imu
 from geometry_msgs.msg import Vector3
 import tf.transformations as trans
 
+class converterClass():
+	def __init__(self):
+		rospy.init_node("converter")
+		nodename = rospy.get_name()
+		rospy.loginfo("node %s has started" % nodename)
+		# subscribers
+		rospy.Subscriber('imu_in', Imu, self.imuCallback)  # change the name of the topic and the varible 
 
-def convertor():
-    rospy.init_node('qToEuler', anonymous = True) # listener ka node 
-    rospy.Subscriber('imu_in', Imu , imuCallback) # imu ka subscriber with the imu message type 
-    rospy.spin()
-
-def yprPublisher():
-    pub = rospy.Publisher('yawPitchRoll', Vector3 ,  queue_size = 20)
-    rate = rospy.Rate(20) # 10hz
-    while not rospy.is_shutdown():
-	rospy.loginfo(ypr)
-        pub.publish(ypr)
-        rate.sleep()
+		# publishers
+		self.eulerAngles = rospy.Publisher('yprEuler', Vector3, queue_size = 10) # change the name of the topic and the data type name 
+	
+		# variables
+		self.q_in = []
+		self.euler = 0
+		self.ypr = Vector3()
+		self.rate = rospy.get_param("~rate", 20)
 
 
-def imuCallback(msg):
-    q_in = [msg.orientation.x,
-	    msg.orientation.y,
-            msg.orientation.z,
-	    msg.orientation.w]
-    euler = trans.euler_from_quaternion(q_in) 
-    ypr = Vector3(euler[2],
-           euler[0],
-	   euler[1])
-    rospy.loginfo(ypr)
+	def spin(self):
+		r = rospy.Rate(self.rate)
+		while not rospy.is_shutdown():
+			self.spinOnce()
+			r.sleep()
+
+	def spinOnce(self):
+		self.eulerAngles.publish(self.ypr)
+
+		
+
+	# callback functions
+	def imuCallback(self, msg):
+		self.q_in = [msg.orientation.x,
+	    		msg.orientation.y,
+            		msg.orientation.z,
+	    		msg.orientation.w]
+    		self.euler = trans.euler_from_quaternion(q_in) 
+    		self.ypr = Vector3(euler[2],
+           			euler[0],
+	   			euler[1])
+		rospy.loginfo(ypr)
+	
 
 if __name__ == '__main__':
-   convertor()
-   yprPublisher()
-
+	try:
+		C = converterClass()
+		C.spin()
+	except rospy.ROSInterruptException:pass
