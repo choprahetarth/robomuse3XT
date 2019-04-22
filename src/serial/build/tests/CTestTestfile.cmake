c1578437d62b5d7966f28a33ff0f9bc10fbd70fc
choprahetarth <choprahetarth@gmail.com>
2019-04-22 13:15:06 +0530

    added mux and imu tools and serial library

---
+++ src/serial/build/tests/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/hetarthvader/robomuse3xt_ws/src/serial/tests
+# Build directory: /home/hetarthvader/robomuse3xt_ws/src/serial/build/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(_ctest_serial_gtest_serial-test "/home/hetarthvader/robomuse3xt_ws/src/serial/build/catkin_generated/env_cached.sh" "/usr/bin/python" "/opt/ros/kinetic/share/catkin/cmake/test/run_tests.py" "/home/hetarthvader/robomuse3xt_ws/src/serial/build/test_results/serial/gtest-serial-test.xml" "--return-code" "/home/hetarthvader/robomuse3xt_ws/src/serial/build/devel/lib/serial/serial-test --gtest_output=xml:/home/hetarthvader/robomuse3xt_ws/src/serial/build/test_results/serial/gtest-serial-test.xml")
+add_test(_ctest_serial_gtest_serial-test-timer "/home/hetarthvader/robomuse3xt_ws/src/serial/build/catkin_generated/env_cached.sh" "/usr/bin/python" "/opt/ros/kinetic/share/catkin/cmake/test/run_tests.py" "/home/hetarthvader/robomuse3xt_ws/src/serial/build/test_results/serial/gtest-serial-test-timer.xml" "--return-code" "/home/hetarthvader/robomuse3xt_ws/src/serial/build/devel/lib/serial/serial-test-timer --gtest_output=xml:/home/hetarthvader/robomuse3xt_ws/src/serial/build/test_results/serial/gtest-serial-test-timer.xml")
