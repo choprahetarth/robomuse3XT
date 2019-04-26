fb852c7c985fa0bf1ebdeb326be3e08831b1ad73
choprahetarth <choprahetarth@gmail.com>
2019-04-26 17:46:42 +0530

    "added the calibration protocol same as robomuse v"

--- src/robomuse_hardware/scripts/calibrate.py
+++ src/robomuse_hardware/scripts/calibrate.py
@@ -7,6 +7,7 @@ from std_msgs.msg import Int32
 from geometry_msgs.msg import Pose2D
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Vector3
 
 class box():
 	def __init__(self):
@@ -19,6 +20,7 @@ class box():
 		rospy.Subscriber('imu', Float32, self.imu_callback)
 		# publishers
 		self.cmd = rospy.Publisher('calibrate/cmd_vel', Twist, queue_size = 10)
+		self.factorVector = rospy.Publisher('calibrationFactors', Vector3, queue_size = 10)
 		# variables
 		self.rate = rospy.get_param("~rate", 10)
 		#values for the box path function
@@ -33,11 +35,15 @@ class box():
 		self.count = 0
 		self.loopFlag = 0
 		self.calibration_protocol = 0
-	def spin(self):
-		r = rospy.Rate(self.rate)
+		self.xFactor = 0
+		self.yFactor = 0
+		self.thetaFactor = 0
+		self.publishOnceFlag = 1
+		self.factors = Vector3()
+	def control_function(self):
 		while not rospy.is_shutdown():
-			#self.spinOnce()
-			r.sleep()
+			cal.self_calibration()
+			
 
 	# callback functions
 	def odometry_callback(self, msg):
@@ -56,44 +62,53 @@ class box():
 		initialY = self.y
 		initialYaw = self.yaw
 		while (self.calibration_protocol == 0):
+			if rospy.is_shutdown():
+				break
 			while (abs(time.time() - self.start) < 2):
 				#print(time.time() - self.start)
-				print(self.count)
 				if (self.count == 0):
 					self.calibrateVel.linear.x = 0.3
-					print("forward")
+					rospy.loginfo_throttle(120,"calibration started 0% ")
 				elif (self.count == 1 or self.count == 2):
 					self.calibrateVel.linear.x = -0.3
-					print("going backward")
+					rospy.loginfo_throttle(120,"calibration started 32% ")
 				elif (self.count == 3):
 					self.calibrateVel.linear.x = 0.3
-					print("going forward again")
+					rospy.loginfo_throttle(120,"calibration started 50%")
 				elif (self.count == 4):
 					self.calibrateVel.linear.x = 0.0
 					self.calibrateVel.angular.z = 0.4
-					print("turning on one side")
+					rospy.loginfo_throttle(120,"calibration started 65%")
 				elif (self.count == 5):
 					self.calibrateVel.linear.x = 0.0
 					self.calibrateVel.angular.z = -0.4
-					print("turning on the other side")
+					rospy.loginfo_throttle(120,"calibration started 80%")
 				elif (self.count == 6):
 					self.calibration_protocol = 1	
-					print("exiting the loop")
+					rospy.loginfo_throttle(120,"calibration complete 100% ")
 					break
 				r = rospy.Rate(self.rate)
 				self.cmd.publish(self.calibrateVel)
 				r.sleep()		
 			self.start = time.time ()
 			self.count = self.count  +  1
-		print ("X ENCODER ==========> ", initialX)
-		print ("Y ENCODER ==========> ", initialY)
-		print ("THETA ==============>" , initialYaw)
-		print ("LASER X ============> ", self.xlaser)
-		print ("LASER Y ============> ", self.ylaser)
-		print ("LASER Z ============> ", self.thetalaser)
+		print("X ENCODER ==========> ", initialX)
+		print("Y ENCODER ==========> ", initialY)
+		print("THETA ==============>" , initialYaw)
+		print("LASER X ============> ", self.xlaser)
+		print("LASER Y ============> ", self.ylaser)
+		print("LASER Z ============> ", self.thetalaser)
+		self.xFactor = initialX/(self.xlaser+10)
+		self.yFactor = initialY/(self.ylaser+10)
+		self.thetaFactor = initialYaw/(self.thetalaser+10)
+		self.factors = Vector3(self.xFactor , self.yFactor, self.thetaFactor)
+		if (self.publishOnceFlag == 1):
+			r = rospy.Rate(self.rate)
+			self.factorVector.publish(self.factors)
+			r.sleep()
+			self.publishOnceFlag = 0
 if __name__ == '__main__':
 	try:
 		cal = box()
-		cal.self_calibration()
-		#cal.spin()
+		cal.control_function()
 	except rospy.ROSInterruptException:pass
